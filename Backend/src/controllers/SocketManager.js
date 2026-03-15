4f1505e0c60d4f59460835593f458858eb23b718
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-03-15 18:16:20 +0530

    first commit

--- Backend/src/controllers/SocketManager.js
+++ Backend/src/controllers/SocketManager.js
@@ -1,8 +1,42 @@
 import { Server } from "socket.io";
 
+let connections ={}
+let messages ={}
+
+let timeOnline={}
+
 
 const intializeSocket = (server)=>{
     const io = new Server(server);
+
+
+    io.on("connections",(socket)=>{
+        socket.on("accept-call",(path)=>{
+
+            if(connections[path] === undefined){
+                connections[path] =[]
+            }
+            connections[path].push(socket.id)
+
+            timeOnline[socket.id] =new Date();
+
+
+            for(let user=0; user<connections[path].length; user++){
+                io.to(connections[path][a]).emit("user-joined")
+            }
+
+        })
+        socket.on("signal",(toId,message)=>{
+            io.to(toId).emit("singal",socket.id,message);
+        })
+        socket.on("chat-message", (data,sender)=>{
+
+        })
+
+        socket.on("disconnect",()=>{
+
+        })
+    })
 
     return io;
 }
